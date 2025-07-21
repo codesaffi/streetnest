2c9050c69b87b5ae7c59d04a0b75f7e80e5e6174
SAFFI ULLAH <165846488+codesaffi@users.noreply.github.com>
2025-07-21 23:16:28 +0500

    Update Hero.jsx

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <div className='text-[#414141]'>
             <div className='flex items-center gap-2'>
                 <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
-                <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
+                <p className='font-medium text-sm md:text-base'>OUR BESTSELLER</p>
             </div>
             <h1 className='prata-regular text-3x1 sm:py-3 lg:text-5x1 leading-relaxed'>Latest Arrivals</h1>
             <div className='flex items-center gap-2'>
